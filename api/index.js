60b76cba0191a290b001c26d79fbcb6218666736
JCPrime01 <vanzokiller@gmail.com>
2026-05-12 17:45:44 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
   export default function handler(req, res) {                                                                                                                                              
-    res.writeHead(302, { Location: 'https://URL_DA_CASA_AQUI' });
+    res.writeHead(302, { Location: 'https://grupojogadorcaro.com.br/quem-e-jota' });
     res.end();                                                                                                                                                                             
   }
